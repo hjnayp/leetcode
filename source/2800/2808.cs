e6b29627d6b340b052bcfd23cc88092bfaebaf73
nayun <han.nayun@icloud.com>
2024-01-30 10:01:32 +0800

    + feat:2808

---
+++ source/2800/2808.cs
@@ -0,0 +1,26 @@
+namespace source._2800._2808;
+
+public class Solution
+{
+    public int MinimumSeconds(IList<int> nums)
+    {
+        int n = nums.Count;
+        var map = new Dictionary<int, List<int>>();
+        for (var i = 0; i < n; ++i)
+        {
+            map.TryAdd(nums[i], new List<int>());
+            map[nums[i]].Add(i);
+        }
+
+        int res = n;
+        foreach ((_, List<int>? positions) in map)
+        {
+            int maxDistance = positions[0] + n - positions[^1];
+            for (var i = 1; i < positions.Count; ++i)
+                maxDistance = Math.Max(maxDistance, positions[i] - positions[i - 1]);
+            res = Math.Min(res, maxDistance / 2);
+        }
+
+        return res;
+    }
+}
