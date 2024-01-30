a406f13141559617c46185586d4a60b4fc61f4ba
nayun <han.nayun@icloud.com>
2024-01-30 09:43:26 +0800

    feat: 2846

---
+++ source/2800/2846.cs
@@ -0,0 +1,100 @@
+namespace source._2800._2846;
+
+public class Solution
+{
+    private const int _W = 26;
+
+    public int[] MinOperationsQueries(int n, int[][] edges, int[][] queries)
+    {
+        int m = queries.Length;
+        var neighbors = new IDictionary<int, int>[n];
+        for (var i = 0; i < n; i++)
+            neighbors[i] = new Dictionary<int, int>();
+
+        foreach (int[] edge in edges)
+        {
+            neighbors[edge[0]].Add(edge[1], edge[2]);
+            neighbors[edge[1]].Add(edge[0], edge[2]);
+        }
+
+        var queryArr = new IList<int[]>[n];
+        for (var i = 0; i < n; i++)
+            queryArr[i] = new List<int[]>();
+
+        for (var i = 0; i < m; i++)
+        {
+            queryArr[queries[i][0]].Add(new[] { queries[i][1], i });
+            queryArr[queries[i][1]].Add(new[] { queries[i][0], i });
+        }
+
+        var count = new int[n][];
+        for (var i = 0; i < n; i++)
+            count[i] = new int[_W + 1];
+
+        var visited = new bool[n];
+        var uf = new int[n];
+        var lca = new int[m];
+        Tarjan(0, -1, neighbors, queryArr, count, visited, uf, lca);
+
+        var res = new int[m];
+        for (var i = 0; i < m; i++)
+        {
+            int totalCount = 0, maxCount = 0;
+            for (var j = 1; j <= _W; j++)
+            {
+                int t = count[queries[i][0]][j] + count[queries[i][1]][j] - 2 * count[lca[i]][j];
+                maxCount = Math.Max(maxCount, t);
+                totalCount += t;
+            }
+
+            res[i] = totalCount - maxCount;
+        }
+
+        return res;
+    }
+
+    private void Tarjan(int node,
+                        int parent,
+                        IDictionary<int, int>[] neighbors,
+                        IList<int[]>[] queryArr,
+                        int[][] count,
+                        bool[] visited,
+                        int[] uf,
+                        int[] lca)
+    {
+        if (parent != -1)
+        {
+            Array.Copy(count[parent], 0, count[node], 0, _W + 1);
+            count[node][neighbors[node][parent]]++;
+        }
+
+        uf[node] = node;
+        foreach (int child in neighbors[node].Keys)
+        {
+            if (child == parent) continue;
+
+            Tarjan(child, node, neighbors, queryArr, count, visited, uf, lca);
+            uf[child] = node;
+        }
+
+        foreach (int[] pair in queryArr[node])
+        {
+            int targetNode = pair[0];
+            if (node != targetNode && !visited[targetNode]) continue;
+            
+            int index = pair[1];
+            lca[index] = Find(uf, targetNode);
+        }
+
+        visited[node] = true;
+    }
+
+    private int Find(int[] uf, int i)
+    {
+        if (uf[i] == i)
+            return i;
+
+        uf[i] = Find(uf, uf[i]);
+        return uf[i];
+    }
+}
---
+++ test/2800/2846.cs
@@ -0,0 +1,37 @@
+using source._2800._2846;
+
+namespace test._2800;
+
+[TestClass]
+public class Test2846
+{
+    private Solution _solution = new();
+
+    [TestMethod]
+    public void NormalCase()
+    {
+        var n = 8;
+
+        var edges = new[]
+        {
+            new[] { 1, 2, 6 },
+            new[] { 1, 3, 4 },
+            new[] { 2, 4, 6 },
+            new[] { 2, 5, 3 },
+            new[] { 3, 6, 6 },
+            new[] { 3, 0, 8 },
+            new[] { 7, 0, 2 },
+        };
+
+        var queries = new[]
+        {
+            new[] { 4, 6 },
+            new[] { 0, 4 },
+            new[] { 6, 5 },
+            new[] { 7, 4 },
+        };
+
+        var expected = new[] { 1, 2, 2, 3 };
+        Assert.IsTrue(expected.SequenceEqual(_solution.MinOperationsQueries(n, edges, queries)));
+    }
+}
