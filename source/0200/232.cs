59c1070e5852fe8ac37792d6826fb8177964d8fe
nayun <han.nayun@icloud.com>
2024-03-04 09:22:57 +0800

    Feat: daily leetcode

---
+++ source/0200/232.cs
@@ -0,0 +1,39 @@
+namespace source._0200._232;
+
+public class MyQueue
+{
+    private Stack<int> _inStack = new();
+    private Stack<int> _outStack = new();
+
+    public void Push(int x)
+    {
+        _inStack.Push(x);
+    }
+
+    public int Pop()
+    {
+        if (_outStack.Count == 0)
+            InToOut();
+
+        return _outStack.Pop();
+    }
+
+    public int Peek()
+    {
+        if (_outStack.Count == 0)
+            InToOut();
+
+        return _outStack.Peek();
+    }
+
+    public bool Empty()
+    {
+        return _inStack.Count + _outStack.Count == 0;
+    }
+
+    private void InToOut()
+    {
+        while (_inStack.Count > 0)
+            _outStack.Push(_inStack.Pop());
+    }
+}
