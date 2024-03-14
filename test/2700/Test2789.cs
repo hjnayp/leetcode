848d3bd9335eec5768e161b87b777879a6494ba8
nayun <han.nayun@icloud.com>
2024-03-14 14:23:23 +0800

    Feat: Daily leetcode

---
+++ test/2700/Test2789.cs
@@ -0,0 +1,25 @@
+using JetBrains.Annotations;
+using source._2700._2789;
+
+namespace test._2700;
+
+[TestClass]
+[TestSubject(typeof(Solution))]
+public class Test2789
+{
+
+    [TestMethod]
+    public void Method()
+    {
+        var solution = new Solution();
+        int[] input = { 2, 3, 7, 9, 3 };
+        long expected = 21;
+        long actual = solution.MaxArrayValue(input);
+        Assert.AreEqual(expected, actual);
+
+        input = new[] { 5, 3, 3 };
+        expected = 11;
+        actual = solution.MaxArrayValue(input);
+        Assert.AreEqual(expected, actual);
+    }
+}
