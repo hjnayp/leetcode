d0841460c75e0f3ce4eff18c9f0cbe0bf7d54e28
nayun <han.nayun@icloud.com>
2024-03-25 09:49:10 +0800

    Style: Rearrange code

--- test/0500/Test518.cs
+++ test/0500/Test518.cs
@@ -7,17 +7,17 @@ namespace test._0500;
 [TestSubject(typeof(Solution))]
 public class Test518
 {
+    private static void RunTest(int amount, int[] coins, int expected)
+    {
+        var solution = new Solution();
+        Assert.AreEqual(expected, solution.Change(amount, coins));
+    }
+
     [TestMethod]
     public void NormalCase()
     {
         RunTest(5, new[] { 1, 2, 5 }, 4);
         RunTest(3, new[] { 2 }, 0);
         RunTest(10, new[] { 10 }, 1);
-    }
-
-    private static void RunTest(int amount, int[] coins, int expected)
-    {
-        var solution = new Solution();
-        Assert.AreEqual(expected, solution.Change(amount, coins));
     }
 }
