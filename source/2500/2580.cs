f8827c7ea425dcf6c28295282549ab032c8ec66e
nayun <han.nayun@icloud.com>
2024-03-27 10:22:23 +0800

    Feat: 3/27

---
+++ source/2500/2580.cs
@@ -0,0 +1,29 @@
+namespace source._2500._2580;
+
+public class Solution
+{
+    public int CountWays(int[][] ranges)
+    {
+        const int MOD = (int)(1e9 + 7);
+        Array.Sort(ranges, (a, b) => a[0] - b[0]);
+
+
+        int n = ranges.Length;
+        long res = 1;
+        for (var i = 0; i < n;)
+        {
+            int right = ranges[i][1];
+            int j = i + 1;
+            while (j < n && ranges[j][0] <= right)
+            {
+                right = Math.Max(right, ranges[j][1]);
+                ++j;
+            }
+
+            res = res * 2 % MOD;
+            i = j;
+        }
+
+        return (int)res;
+    }
+}
