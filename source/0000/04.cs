f2d1f4f7e5b6c548322881260ab85e2c79284f73
nayun <han.nayun@icloud.com>
2024-04-01 11:04:15 +0800

    Feat: 04

---
+++ source/0000/04.cs
@@ -0,0 +1,43 @@
+namespace source._0000;
+
+/// <summary>
+///     https://leetcode.cn/problems/median-of-two-sorted-arrays/description/
+/// </summary>
+public class Solution
+{
+    public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+    {
+        int m = nums1.Length;
+        int n = nums2.Length;
+        int len = m + n;
+        int k1 = (len + 1) / 2;
+        int k2 = (len + 2) / 2;
+
+        return (FindKthValue(k1) + FindKthValue(k2)) / 2f;
+
+        int FindKthValue(int k)
+        {
+            var i = 0;
+            var j = 0;
+            while (true)
+            {
+                if (i == m) return nums2[j + k - 1];
+                if (j == n) return nums1[i + k - 1];
+                if (k == 1) return Math.Min(nums1[i], nums2[j]);
+
+                int idx1 = Math.Min(m - 1, i + k / 2 - 1);
+                int idx2 = Math.Min(n - 1, j + k / 2 - 1);
+                if (nums1[idx1] <= nums2[idx2])
+                {
+                    k -= idx1 - i + 1;
+                    i = idx1 + 1;
+                }
+                else
+                {
+                    k -= idx2 - j + 1;
+                    j = idx2 + 1;
+                }
+            }
+        }
+    }
+}
---
+++ test/0000/Test04.cs
@@ -0,0 +1,31 @@
+using JetBrains.Annotations;
+using source._0000;
+
+namespace test._0000;
+
+[TestClass]
+[TestSubject(typeof(Solution))]
+public class Test04
+{
+    [TestMethod]
+    public void NormalCase()
+    {
+        var solution = new Solution();
+        var nums1 = new[] { 1, 3 };
+        var nums2 = new[] { 2 };
+        Assert.AreEqual(2.0, solution.FindMedianSortedArrays(nums1, nums2));
+
+        nums1 = new[] { 1, 2 };
+        nums2 = new[] { 3, 4 };
+        Assert.AreEqual(2.5, solution.FindMedianSortedArrays(nums1, nums2));
+    }
+
+    [TestMethod]
+    public void EmptyListCase()
+    {
+        var solution = new Solution();
+        var nums1 = new int[] { };
+        var nums2 = new[] { 2, 3 };
+        Assert.AreEqual(2.5, solution.FindMedianSortedArrays(nums1, nums2));
+    }
+}
