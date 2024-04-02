1dc6d0e16526c43a6dc3f1a1963500d7ed865068
nayun <han.nayun@icloud.com>
2024-04-02 10:45:40 +0800

    Test: Cover 04

--- test/0000/Test04.cs
+++ test/0000/Test04.cs
@@ -18,6 +18,10 @@ public class Test04
         nums1 = new[] { 1, 2 };
         nums2 = new[] { 3, 4 };
         Assert.AreEqual(2.5, solution.FindMedianSortedArrays(nums1, nums2));
+
+        nums1 = new[] { 10, 20 };
+        nums2 = new[] { 3, 4 };
+        Assert.AreEqual(7, solution.FindMedianSortedArrays(nums1, nums2));
     }
 
     [TestMethod]
@@ -27,5 +31,9 @@ public class Test04
         var nums1 = new int[] { };
         var nums2 = new[] { 2, 3 };
         Assert.AreEqual(2.5, solution.FindMedianSortedArrays(nums1, nums2));
+
+        nums1 = new[] { 1, 9 };
+        nums2 = new int[] { };
+        Assert.AreEqual(5.0, solution.FindMedianSortedArrays(nums1, nums2));
     }
 }
