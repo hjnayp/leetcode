2e7c1080bba3b9c43f4e7976e3e14dde76383c78
nayun <han.nayun@icloud.com>
2024-04-03 13:08:26 +0800

    Update: 添加注释

--- source/1300/1379.cs
+++ source/1300/1379.cs
@@ -2,6 +2,9 @@ using source.Structs;
 
 namespace source._1300._1379;
 
+/// <summary>
+///     https://leetcode.cn/problems/find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/description
+/// </summary>
 public class Solution
 {
     public TreeNode? GetTargetCopy(TreeNode? original, TreeNode? cloned, TreeNode? target)
