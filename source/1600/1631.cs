441d0f119e626fe52f6c9a6611a44e63d2a64b12
nayun <han.nayun@icloud.com>
2024-04-10 09:12:52 +0800

    Style: Update style

--- source/1600/1631.cs
+++ source/1600/1631.cs
@@ -2,34 +2,34 @@ namespace source._1600._1631;
 
 public class Solution
 {
-    private static readonly Tuple<int, int>[] Directions = { new(0, 1), new(0, -1), new(1, 0), new(-1, 0) };
+    private static readonly Tuple<int, int>[] s_Directions = { new(0, 1), new(0, -1), new(1, 0), new(-1, 0) };
 
     public int MinimumEffortPath(int[][] heights)
     {
-        var m = heights.Length;
-        var n = heights[0].Length;
+        int m = heights.Length;
+        int n = heights[0].Length;
         var left = 0;
         var right = 1000000;
         var res = 0;
         while (left <= right)
         {
-            var mid = left + (right - left) / 2;
+            int mid = left + (right - left) / 2;
             var queue = new Queue<Tuple<int, int>>();
             queue.Enqueue(new Tuple<int, int>(0, 0));
             var visited = new bool[m, n];
             visited[0, 0] = true;
             while (queue.Any())
             {
-                var (x, y) = queue.Dequeue();
-                foreach (var direction in Directions)
+                (int x, int y) = queue.Dequeue();
+                foreach (Tuple<int, int> direction in s_Directions)
                 {
-                    var new_x = x + direction.Item1;
-                    var new_y = y + direction.Item2;
-                    if (new_x < 0 || new_x >= m || new_y < 0 || new_y >= n) continue;
-                    if (visited[new_x, new_y] || Math.Abs(heights[x][y] - heights[new_x][new_y]) > mid) continue;
+                    int newX = x + direction.Item1;
+                    int newY = y + direction.Item2;
+                    if (newX < 0 || newX >= m || newY < 0 || newY >= n) continue;
+                    if (visited[newX, newY] || Math.Abs(heights[x][y] - heights[newX][newY]) > mid) continue;
 
-                    queue.Enqueue(new Tuple<int, int>(new_x, new_y));
-                    visited[new_x, new_y] = true;
+                    queue.Enqueue(new Tuple<int, int>(newX, newY));
+                    visited[newX, newY] = true;
                 }
             }
 
