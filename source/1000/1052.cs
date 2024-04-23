480d96b95b94858861637053f2c11644e320b635
nayun <HanNayun_china@outlook.com>
2024-04-23 09:52:28 +0800

    Feat: 4/23

---
+++ source/1000/1052.cs
@@ -0,0 +1,31 @@
+namespace source._1000._1052;
+
+public class Solution
+{
+    public int MaxSatisfied(int[] customers, int[] grumpy, int minutes)
+    {
+        var left = 0;
+        var right = 0;
+        var maxSatisfied = 0;
+        while (right < minutes)
+        {
+            maxSatisfied += (0 ^ grumpy[right]) * customers[right];
+            ++right;
+        }
+
+        int satisfied = maxSatisfied;
+        int len = customers.Length;
+        while (right < len)
+        {
+            satisfied += (0 ^ grumpy[right]) * customers[right];
+            satisfied -= (0 ^ grumpy[left]) * customers[left];
+            maxSatisfied = Math.Max(maxSatisfied, satisfied);
+            ++right;
+            ++left;
+        }
+
+        maxSatisfied += customers.Select((t, i) => (1 ^ grumpy[i]) * t)
+                                 .Sum();
+        return maxSatisfied;
+    }
+}
