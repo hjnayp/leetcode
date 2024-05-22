e17e5e608b887abd17c2e1859d0843ac0e3284e2
nayun <HanNayun_china@outlook.com>
2024-05-22 10:43:29 +0800

    feature: 5/22

---
+++ source/2200/2225.cs
@@ -0,0 +1,38 @@
+namespace source._2200;
+
+public class Solution
+{
+    public IList<IList<int>> FindWinners(int[][] matches)
+    {
+        var winCnt = new Dictionary<int, int>();
+        var loseCnt = new Dictionary<int, int>();
+
+        foreach (int[] match in matches)
+        {
+            int winner = match[0];
+            int loser = match[1];
+
+            if (!winCnt.TryAdd(winner, 1)) ++winCnt[winner];
+            if (!loseCnt.TryAdd(loser, 1)) ++loseCnt[loser];
+        }
+
+        var res = new List<List<int>> { new(), new() };
+
+        foreach ((int key, int value) in loseCnt)
+        {
+            if (value == 1) res[1].Add(key);
+        }
+
+        foreach ((int key, int value) in winCnt)
+        {
+            if (!loseCnt.ContainsKey(key)) res[0].Add(key);
+        }
+
+        foreach (List<int> arr in res)
+        {
+            arr.Sort();
+        }
+
+        return new int[][] { res[0].ToArray(), res[1].ToArray() };
+    }
+}
