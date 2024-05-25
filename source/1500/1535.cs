135f099b63262dbf47ed550bd5a11a5a14e4f0fd
Nayun <Han.Nayun@icloud.com>
2024-05-25 21:02:13 +0800

    feature: daily

---
+++ source/1500/1535.cs
@@ -0,0 +1,28 @@
+﻿namespace source._1500._1535;
+
+public class Solution
+{
+    public int GetWinner(int[] arr, int k)
+    {
+        int max = arr[0];
+        int consecutiveWins = 0;
+        int idx = 1;
+        while (consecutiveWins < k && idx < arr.Length)
+        {
+            int curVal = arr[idx];
+            if (curVal > max)
+            {
+                max = curVal;
+                consecutiveWins = 1;
+            }
+            else
+            {
+                ++consecutiveWins;
+            }
+
+            ++idx;
+        }
+
+        return max;
+    }
+}
