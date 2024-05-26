9d632e9d1bbcf1e4bdd54b8ea993821612bb7d4b
Nayun <Han.Nayun@icloud.com>
2024-05-26 13:21:19 +0800

    feature: Daily leetcode

---
+++ source/0200/215.cs
@@ -0,0 +1,45 @@
+﻿namespace source._0200._215;
+
+public class Solution
+{
+    public int FindKthLargest(int[] nums, int k)
+    {
+        int n = nums.Length;
+        QuickSelectSort(nums, 0, n - 1, n - k);
+        return nums[^k];
+    }
+
+    private void QuickSelectSort(int[] nums, int left, int right, int k)
+    {
+        if (left >= right) return;
+        int l = left - 1;
+        int r = right + 1;
+        int pivot = nums[(left + right) / 2];
+        while (l < r)
+        {
+            do
+            {
+                ++l;
+            } while (nums[l] < pivot);
+
+            do
+            {
+                --r;
+            } while (nums[r] > pivot);
+
+            if (l < r)
+            {
+                (nums[l], nums[r]) = (nums[r], nums[l]);
+            }
+        }
+
+        if (k <= r)
+        {
+            QuickSelectSort(nums, left, r, k);
+        }
+        else
+        {
+            QuickSelectSort(nums, r + 1, right, k);
+        }
+    }
+}
---
+++ test/1700/Test1738.cs
@@ -0,0 +1,23 @@
+﻿using JetBrains.Annotations;
+using source._1700._1738;
+
+namespace test._1700;
+
+[TestClass, TestSubject(typeof(Solution))]
+public class Test1738
+{
+    [TestMethod]
+    public void normal_case()
+    {
+        var solution = new Solution();
+        int[][] matrix = { new[] { 5, 2 }, new[] { 1, 6 } };
+        int k = 1;
+        Assert.AreEqual(7, solution.KthLargestValue(matrix, k));
+
+        matrix = new[] { new[] { 5, 2 }, new[] { 1, 6 } };
+        k = 2;
+        Assert.AreEqual(5, solution.KthLargestValue(matrix, k));
+        k = 3;
+        Assert.AreEqual(4, solution.KthLargestValue(matrix, k));
+    }
+}
