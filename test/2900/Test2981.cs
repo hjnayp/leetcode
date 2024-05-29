89a4d1a1ee5033db6e8c97e15191d3d9e0c54597
nayun <HanNayun_china@outlook.com>
2024-05-29 10:15:54 +0800

    feature: Daily leetcode

---
+++ test/2900/Test2981.cs
@@ -0,0 +1,33 @@
+using JetBrains.Annotations;
+using source._2900._2981;
+
+namespace test._2900;
+
+[TestClass]
+[TestSubject(typeof(Solution))]
+public class Test2981
+{
+    [TestMethod]
+    public void normal_case()
+    {
+        var solution = new Solution();
+        Assert.AreEqual(2, solution.MaximumLength("aaaa"));
+        Assert.AreEqual(1, solution.MaximumLength("abcaba"));
+    }
+
+    [TestMethod]
+    public void special_case_01()
+    {
+        var solution = new Solution();
+        Assert.AreEqual(1, solution.MaximumLength("eccdnmcnkl"));
+        Assert.AreEqual(4, solution.MaximumLength("lkwwdddddnnnnnynnnnl"));
+    }
+
+    [TestMethod]
+    public void no_special_substring_occur()
+    {
+        var solution = new Solution();
+        Assert.AreEqual(-1, solution.MaximumLength("abcdef"));
+        Assert.AreEqual(-1, solution.MaximumLength("acc"));
+    }
+}
