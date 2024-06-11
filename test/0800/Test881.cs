6c495eaa9e0e6bc6ca29948b6c1af0fac0f766c7
nayun <HanNayun_china@outlook.com>
2024-06-11 09:14:59 +0800

    feature: 881

---
+++ test/0800/Test881.cs
@@ -0,0 +1,19 @@
+using JetBrains.Annotations;
+using source._0800._881;
+using source.InputParsers;
+
+namespace test._0800;
+
+[TestClass]
+[TestSubject(typeof(Solution))]
+public class Test881
+{
+    [TestMethod]
+    public void normal_case()
+    {
+        Solution solution = new();
+        Assert.AreEqual(1, solution.NumRescueBoats(ArrayParser.ParseOneDimensionalArray("[1,2]"), 3));
+        Assert.AreEqual(3, solution.NumRescueBoats(ArrayParser.ParseOneDimensionalArray("[3,2,2,1]"), 3));
+        Assert.AreEqual(4, solution.NumRescueBoats(ArrayParser.ParseOneDimensionalArray("[3,5,3,4]"), 5));
+    }
+}
