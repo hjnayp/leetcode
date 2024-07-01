49acb97e49ddfbe48037f243805bc3a637f8c11c
nayun <HanNayun_china@outlook.com>
2024-07-01 10:07:37 +0800

    update: Update the comment, remove unnecessary part

--- source/InputParsers/ArrayParser.cs
+++ source/InputParsers/ArrayParser.cs
@@ -3,12 +3,8 @@ namespace source.InputParsers;
 public static class ArrayParser
 {
     /// <summary>
-    ///     Convert the input string to an one-dimensional integer array
+    ///     Convert the input string to a one-dimensional array
     /// </summary>
-    /// <param name="input">
-    ///     The input string that represent an one-dimensional array
-    /// </param>
-    /// <returns></returns>
     /// <example>
     ///     input = "[2,0,0,0,0,0,2]"
     ///     return = new []{2,0,0,0,0,0,2}
@@ -28,12 +24,8 @@ public static class ArrayParser
     }
 
     /// <summary>
-    ///     Convert the input string to a two-dimensional integer array
+    ///     Convert the input string to a two-dimensional array
     /// </summary>
-    /// <param name="input">
-    ///     The string that represent a two-dimensional array
-    /// </param>
-    /// <returns></returns>
     /// <example>
     ///     input = "[[2,0,0,0,0,0,2],[1,2,3,4,5,6,7]]"
     ///     input = "[[]]"
