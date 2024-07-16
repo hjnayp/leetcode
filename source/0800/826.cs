065cf07638ef1d7676aa0a5b0729cc1139857ea2
nayun <HanNayun_china@outlook.com>
2024-07-16 09:47:39 +0800

    update: coverage all code

--- source/0800/826.cs
+++ source/0800/826.cs
@@ -1,36 +1,33 @@
 namespace source._0800._826;
 
+/// <summary>
+///     https://leetcode.cn/problems/most-profit-assigning-work
+/// </summary>
 public class Solution
 {
     public int MaxProfitAssignment(int[] difficulty, int[] profit, int[] worker)
     {
-        Array.Sort(worker, (a, b) => b - a);
-        int n = difficulty.Length;
-        var jobs = new (int difficulty, int profit)[n];
-        for (int i = 0; i < n; i++)
-        {
-            jobs[i] = (difficulty[i], profit[i]);
-        }
+        int difficultyLen = difficulty.Length;
+
+        var jobs = new (int difficulty, int profit)[difficultyLen];
+        for (int i = 0; i < difficultyLen; i++) jobs[i] = (difficulty[i], profit[i]);
 
         Array.Sort(jobs, (a, b) =>
         {
             if (a.profit == b.profit) return b.difficulty - a.difficulty;
             return b.profit - a.profit;
         });
+        Array.Sort(worker, (a, b) => b - a);
 
-        int res = 0;
+        int maxProfit = 0;
         int idx = 0;
         foreach (int w in worker)
         {
-            while (idx < n && w < jobs[idx].difficulty)
-            {
-                ++idx;
-            }
-
-            if (idx == n) break;
-            res += jobs[idx].profit;
+            while (idx < difficultyLen && w < jobs[idx].difficulty) ++idx;
+            if (idx == difficultyLen) break;
+            maxProfit += jobs[idx].profit;
         }
 
-        return res;
+        return maxProfit;
     }
 }
--- test/0800/Test826.cs
+++ test/0800/Test826.cs
@@ -10,20 +10,30 @@ public class Test826
     [TestMethod]
     public void normal_case()
     {
-        var solution = new Solution();
-        int[] difficulty = { 2, 4, 6, 8, 10 };
-        int[] profit = { 10, 20, 30, 40, 50 };
-        int[] worker = { 4, 5, 6, 7 };
+        Solution solution = new();
+        int[] difficulty = [2, 4, 6, 8, 10];
+        int[] profit = [10, 20, 30, 40, 50];
+        int[] worker = [4, 5, 6, 7];
         Assert.AreEqual(100, solution.MaxProfitAssignment(difficulty, profit, worker));
+    }
+
+    [TestMethod]
+    public void all_profit_difficulty_work_same()
+    {
+        Solution solution = new();
+        int[] difficulty = [10, 10, 10, 10];
+        int[] profit = [10, 10, 10, 10];
+        int[] worker = [10, 10, 10, 10];
+        Assert.AreEqual(40, solution.MaxProfitAssignment(difficulty, profit, worker));
     }
 
     [TestMethod]
     public void gain_no_profit_case()
     {
-        var solution = new Solution();
-        int[] difficulty = { 85, 47, 57 };
-        int[] profit = { 24, 66, 99 };
-        int[] worker = { 40, 25, 25 };
+        Solution solution = new();
+        int[] difficulty = [85, 47, 57];
+        int[] profit = [24, 66, 99];
+        int[] worker = [40, 25, 25];
         Assert.AreEqual(0, solution.MaxProfitAssignment(difficulty, profit, worker));
     }
 }
