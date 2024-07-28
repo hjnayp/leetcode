a1362c0c6fb6692b86ba410140afe06754352dd8
Nayun <Han.Nayun@icloud.com>
2024-07-28 23:16:39 +0800

    + 699

---
+++ source/0600/699.cs
@@ -0,0 +1,34 @@
+﻿namespace source._0600._699;
+
+/// <summary>
+///     https://leetcode.cn/problems/falling-squares
+/// </summary>
+public class Solution
+{
+    public IList<int> FallingSquares(int[][] positions)
+    {
+        IList<int> heights = new List<int>();
+        for (int i = 0; i < positions.Length; i++)
+        {
+            int left = positions[i][0];
+            int sideLength = positions[i][1];
+            int right = left + sideLength - 1;
+            heights.Add(sideLength);
+
+            for (int j = 0; j < i; ++j)
+            {
+                int left2 = positions[j][0];
+                int right2 = positions[j][0] + positions[j][1] - 1;
+                if (left2 > right || right2 < left) continue;
+                heights[i] = Math.Max(heights[i], heights[j] + sideLength);
+            }
+        }
+
+        for (int i = 1; i < heights.Count; i++)
+        {
+            heights[i] = Math.Max(heights[i], heights[i - 1]);
+        }
+
+        return heights;
+    }
+}
---
+++ test/0600/Test699.cs
@@ -0,0 +1,28 @@
+﻿using JetBrains.Annotations;
+using source._0600._699;
+
+namespace test._0600;
+
+[TestClass, TestSubject(typeof(Solution))]
+public class Test699
+{
+    [TestMethod]
+    public void normal_case()
+    {
+        var solution = new Solution();
+        int[][] positions = [[1, 2], [2, 3], [6, 1]];
+        int[] result = solution.FallingSquares(positions).ToArray();
+        int[] expected = [2, 5, 5];
+        CollectionAssert.AreEqual(expected, result);
+    }
+
+    [TestMethod]
+    public void normal_case_1()
+    {
+        var solution = new Solution();
+        int[][] positions = [[100, 100], [200, 100]];
+        int[] result = solution.FallingSquares(positions).ToArray();
+        int[] expected = [100, 100];
+        CollectionAssert.AreEqual(expected, result);
+    }
+}
