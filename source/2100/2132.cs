a91ce5150c52e683a74d64d5a16802ef8bc8b81c
Nayun <Han.Nayun@icloud.com>
2024-07-29 20:52:06 +0800

    $ format 2132

--- source/2100/2132.cs
+++ source/2100/2132.cs
@@ -4,27 +4,29 @@ public class Solution
 {
     public bool PossibleToStamp(int[][] grid, int stamp_height, int stamp_width)
     {
-        var m = grid.Length;
-        var n = grid[0].Length;
+        int m = grid.Length;
+        int n = grid[0].Length;
 
-        var sum = new int[m + 2][];
-        var diff = new int[m + 2][];
+        int[][] sum = new int[m + 2][];
+        int[][] diff = new int[m + 2][];
 
-        for (var i = 0; i < m + 2; i++)
+        for (int i = 0; i < m + 2; i++)
         {
             sum[i] = new int[n + 2];
             diff[i] = new int[n + 2];
         }
 
-        for (var i = 1; i <= m; i++)
-        for (var j = 1; j <= n; j++)
-            sum[i][j] = sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1] + grid[i - 1][j - 1];
-
-        for (var i = 1; i + stamp_height - 1 <= m; i++)
-        for (var j = 1; j + stamp_width - 1 <= n; j++)
+        for (int i = 1; i <= m; i++)
+        for (int j = 1; j <= n; j++)
         {
-            var x = i + stamp_height - 1;
-            var y = j + stamp_width - 1;
+            sum[i][j] = sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1] + grid[i - 1][j - 1];
+        }
+
+        for (int i = 1; i + stamp_height - 1 <= m; i++)
+        for (int j = 1; j + stamp_width - 1 <= n; j++)
+        {
+            int x = i + stamp_height - 1;
+            int y = j + stamp_width - 1;
             if (sum[x][y] - sum[x][j - 1] - sum[i - 1][y] + sum[i - 1][j - 1] != 0) continue;
 
             diff[i][j]++;
@@ -33,12 +35,14 @@ public class Solution
             diff[x + 1][y + 1]++;
         }
 
-        for (var i = 1; i <= m; i++)
-        for (var j = 1; j <= n; j++)
+        for (int i = 1; i <= m; i++)
+        for (int j = 1; j <= n; j++)
         {
             diff[i][j] += diff[i - 1][j] + diff[i][j - 1] - diff[i - 1][j - 1];
             if (diff[i][j] == 0 && grid[i - 1][j - 1] == 0)
+            {
                 return false;
+            }
         }
 
         return true;
