002f25cf517a35b2abe65dc11c4b3ef5989dcef2
Nayun <Han.Nayun@icloud.com>
2024-08-03 23:25:36 +0800

    + 3134

---
+++ source/3100/3143.cs
@@ -0,0 +1,31 @@
+﻿namespace source._3100._3143;
+
+public class Solution
+{
+    private const int MaxSideLength = (int)1e9 + 1;
+
+    public int MaxPointsInsideSquare(int[][] points, string s)
+    {
+        int[] tagMaxDistances = new int[26];
+        Array.Fill(tagMaxDistances, MaxSideLength);
+        int minDistance = MaxSideLength;
+        for (int i = 0; i < points.Length; i++)
+        {
+            int x = points[i][0];
+            int y = points[i][1];
+            int tag = s[i] - 'a';
+            int distance = Math.Max(Math.Abs(x), Math.Abs(y));
+            if (distance < tagMaxDistances[tag])
+            {
+                minDistance = Math.Min(minDistance, tagMaxDistances[tag]);
+                tagMaxDistances[tag] = distance;
+            }
+            else if (distance < minDistance)
+            {
+                minDistance = distance;
+            }
+        }
+
+        return tagMaxDistances.Count(distance => distance < minDistance);
+    }
+}
