514c32d6b22cd981ea19c70f7883a6887ebb62f2
Nayun <Han.Nayun@icloud.com>
2024-08-04 12:16:08 +0800

    + 572

---
+++ source/0500/572.cs
@@ -0,0 +1,43 @@
+﻿using source.Structs;
+
+namespace source._0500._572;
+
+/// <summary>
+///     https://leetcode.cn/problems/subtree-of-another-tree
+/// </summary>
+public class Solution
+{
+    private const string LeftNull = ",left";
+    private const string RightNull = ",right";
+
+    public bool IsSubtree(TreeNode root, TreeNode subRoot)
+    {
+        PreOrder(root, out string rootPath);
+        PreOrder(subRoot, out string subRootPath);
+        return rootPath.Contains(subRootPath);
+
+        void PreOrder(TreeNode node, out string path)
+        {
+            path = "," + node.val;
+            if (node.left is null)
+            {
+                path += LeftNull;
+            }
+            else
+            {
+                PreOrder(node.left, out string leftPath);
+                path += leftPath;
+            }
+
+            if (node.right is null)
+            {
+                path += RightNull;
+            }
+            else
+            {
+                PreOrder(node.right, out string rightPath);
+                path += rightPath;
+            }
+        }
+    }
+}
---
+++ test/0500/Test572.cs
@@ -0,0 +1,45 @@
+﻿using JetBrains.Annotations;
+using source._0500._572;
+using source.Structs;
+
+namespace test._0500;
+
+[TestClass, TestSubject(typeof(Solution))]
+public class Test572
+{
+    private readonly Solution _solution = new();
+
+    [TestMethod]
+    public void NormalCase()
+    {
+        var rootTree = TreeNode.CreateTreeWithList([3, 4, 5, 1, 2]);
+        var subRootTree = TreeNode.CreateTreeWithList([4, 1, 2]);
+        Assert.IsTrue(_solution.IsSubtree(rootTree, subRootTree));
+
+        rootTree = TreeNode.CreateTreeWithList([1, 1]);
+        subRootTree = TreeNode.CreateTreeWithList([1]);
+        Assert.IsTrue(_solution.IsSubtree(rootTree, subRootTree));
+
+        rootTree = TreeNode.CreateTreeWithList([3, 4, 5, 1, 2, null, null, null, null, 0]);
+        subRootTree = TreeNode.CreateTreeWithList([4, 1, 2]);
+        Assert.IsFalse(_solution.IsSubtree(rootTree, subRootTree));
+
+        rootTree = TreeNode.CreateTreeWithList([1, 2, 3]);
+        subRootTree = TreeNode.CreateTreeWithList([1, 2]);
+        Assert.IsFalse(_solution.IsSubtree(rootTree, subRootTree));
+
+        rootTree = TreeNode.CreateTreeWithList([3, 4, 5, 1, null, 2]);
+        subRootTree = TreeNode.CreateTreeWithList([3, 1, 2]);
+        Assert.IsFalse(_solution.IsSubtree(rootTree, subRootTree));   
+        
+        rootTree = TreeNode.CreateTreeWithList([12]);
+        subRootTree = TreeNode.CreateTreeWithList([2]);
+        Assert.IsFalse(_solution.IsSubtree(rootTree, subRootTree));
+
+        rootTree = TreeNode.CreateTreeWithList([
+            4, -9, 5, null, -1, null, 8, -6, 0, 7, null, null, -2, null, null, null, null, -3
+        ]);
+        subRootTree = TreeNode.CreateTreeWithList([5]);
+        Assert.IsFalse(_solution.IsSubtree(rootTree, subRootTree));
+    }
+}
