e17bcd2f4024fccfa38a27ae91746153973560b2
Nayun <Han.Nayun@icloud.com>
2024-08-11 16:51:33 +0800

    +1035

---
+++ source/1000/1035.cs
@@ -0,0 +1,31 @@
+﻿namespace source._1000._1035;
+
+public class Solution
+{
+    public int MaxUncrossedLines(int[] nums1, int[] nums2)
+    {
+        int m = nums1.Length;
+        int n = nums2.Length;
+        int[,] lineCounts = new int[m + 1, n + 1];
+
+        for (int i = 1; i <= m; ++i)
+        for (int j = 1; j <= n; ++j)
+        {
+            if (IsDrawLineAble(i, j))
+            {
+                lineCounts[i, j] = lineCounts[i - 1, j - 1] + 1;
+            }
+            else
+            {
+                lineCounts[i, j] = Math.Max(lineCounts[i, j - 1], lineCounts[i - 1, j]);
+            }
+        }
+
+        return lineCounts[m, n];
+
+        bool IsDrawLineAble(int index1From1, int index2From1)
+        {
+            return nums1[index1From1 - 1] == nums2[index2From1 - 1];
+        }
+    }
+}   
---
+++ test/1000/Test1035.cs
@@ -0,0 +1,26 @@
+﻿using JetBrains.Annotations;
+using source._1000._1035;
+
+namespace test._1000;
+
+[TestClass, TestSubject(typeof(Solution))]
+public class Test1035
+{
+
+    [TestMethod]
+    public void Method_WhenCommonCase_ShouldPass()
+    {
+        var solution = new Solution();
+        int[] nums1 = [1, 4, 2];
+        int[] nums2 = [1, 2, 4];
+        Assert.AreEqual(2, solution.MaxUncrossedLines(nums1, nums2));
+        
+        nums1 = [2, 5, 1, 2, 5];
+        nums2 = [10, 5, 2, 1, 5, 2];
+        Assert.AreEqual(3,solution.MaxUncrossedLines(nums1, nums2));
+
+        nums1 = [1, 3, 7, 1, 7, 5];
+        nums2 = [1, 9, 2, 5, 1];
+        Assert.AreEqual(2, solution.MaxUncrossedLines(nums1, nums2));
+    }
+}
