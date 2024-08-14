359b9c77bc6dda30842a87abc377398f7049b49d
nayun <HanNayun_china@outlook.com>
2024-08-14 12:08:54 +0800

    ! Cover all code

--- source/0000/15.cs
+++ source/0000/15.cs
@@ -13,7 +13,7 @@ public class Solution
         for (int i = 0; i < n - 2; ++i)
         {
             if (i > 0 && nums[i] == nums[i - 1]) continue;
-            
+
             int j = i + 1;
             int k = n - 1;
 
@@ -23,8 +23,17 @@ public class Solution
                 if (sum == 0)
                 {
                     results.Add(new List<int> { nums[i], nums[j], nums[k] });
-                    while (j < k && nums[j] == nums[j + 1]) ++j;
-                    while (j < k && nums[k] == nums[k - 1]) --k;
+                    
+                    while (j < k && nums[j] == nums[j + 1])
+                    {
+                        ++j;
+                    }
+
+                    while (j < k && nums[k] == nums[k - 1])
+                    {
+                        --k;
+                    }
+
                     ++j;
                     --k;
                 }
--- test/0000/Test15.cs
+++ test/0000/Test15.cs
@@ -10,24 +10,28 @@ public class Test15
     [TestMethod]
     public void TestSolution()
     {
-        var solution = new Solution();
+        Solution solution = new();
         int[] nums = [-1, 0, 1, 2, -1, -4];
         int[][] expecteds = [[-1, -1, 2], [-1, 0, 1]];
         AssertIsAsExpected(expecteds, solution.ThreeSum(nums));
 
-        nums = [0, 1, 1];
+        nums = [0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2];
         expecteds = [];
         AssertIsAsExpected(expecteds, solution.ThreeSum(nums));
-        
+
+        nums = [-2, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2];
+        expecteds = [[-2, 0, 2], [-2, 1, 1]];
+        AssertIsAsExpected(expecteds, solution.ThreeSum(nums));
+
         nums = [0, 0, 0];
         expecteds = [[0, 0, 0]];
         AssertIsAsExpected(expecteds, solution.ThreeSum(nums));
     }
 
-    void AssertIsAsExpected(IList<IList<int>> expected, IList<IList<int>> actual)
+    private void AssertIsAsExpected(IList<IList<int>> expected, IList<IList<int>> actual)
     {
         Assert.AreEqual(expected.Count, actual.Count);
-        for (var i = 0; i < expected.Count; i++)
+        for (int i = 0; i < expected.Count; i++)
         {
             CollectionAssert.AreEqual(expected[i].ToArray(), actual[i].ToArray());
         }
