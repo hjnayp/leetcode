f56cce315aa368b0edad70c38c219e4b6208b6bf
Nayun <Han.Nayun@icloud.com>
2024-08-17 21:11:56 +0800

    + 17

---
+++ test/0000/Test17.cs
@@ -0,0 +1,23 @@
+﻿using JetBrains.Annotations;
+using source._0000._17;
+
+namespace test._0000;
+
+[TestClass, TestSubject(typeof(Solution))]
+public class Test17
+{
+
+    [TestMethod]
+    public void NormalCase()
+    {
+        var solution = new Solution();
+        string[] expected = ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"];
+        CollectionAssert.AreEqual(expected, solution.LetterCombinations("23").ToArray());
+
+        expected = [];
+        CollectionAssert.AreEqual(expected, solution.LetterCombinations("").ToArray());
+        
+        expected = ["a", "b", "c"];
+        CollectionAssert.AreEqual(expected, solution.LetterCombinations("2").ToArray());
+    }
+}
