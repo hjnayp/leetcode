66beb5d1212b482ef18efe978c9634845d4b7a92
Nayun <Han.Nayun@icloud.com>
2024-08-21 09:14:09 +0800

    + 551

---
+++ source/0500/551.cs
@@ -0,0 +1,35 @@
+﻿namespace source._0500._551;
+
+public class Solution
+{
+    public bool CheckRecord(string s)
+    {
+        int absentCount = 0;
+        int lateCount = 0;
+        foreach (char ch in s)
+        {
+            if (ch is 'L')
+            {
+                lateCount++;
+                if (lateCount >= 3)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                lateCount = 0;
+            }
+
+            if (ch is not 'A') continue;
+
+            absentCount++;
+            if (absentCount >= 2)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
