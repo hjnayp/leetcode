ffa93cd65c3926500ed66780fdaf37f8dd393bc9
nayun <HanNayun_china@outlook.com>
2024-09-04 11:11:03 +0800

    + 24

---
+++ source/0000/24.cs
@@ -0,0 +1,32 @@
+using source.Structs;
+
+namespace source._0000._24;
+
+public class Solution
+{
+    public ListNode? SwapPairs(ListNode? head)
+    {
+        if (head is null)
+        {
+            return null;
+        }
+
+        var dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode? current = dummy;
+        while (head?.next is not null)
+        {
+            ListNode? temp = head;
+            head = head.next;
+            current.next = head;
+            temp.next = head.next;
+            head.next = temp;
+            head = temp.next;
+            current = current.next?.next;
+        }
+
+        ListNode? res = dummy.next;
+        dummy.next = null;
+        return res;
+    }
+}
