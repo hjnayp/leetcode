de6f5792e019ed6cbc03cbcb44351cca30633961
nayun <HanNayun_china@outlook.com>
2024-09-05 14:55:58 +0800

    + 26

---
+++ source/0000/26.cs
@@ -0,0 +1,31 @@
+namespace source._0000._26;
+
+/// <summary>
+///     <a href="https://leetcode.cn/problems/remove-duplicates-from-sorted-array/">
+///         Remove Duplicates from Sorted Array
+///     </a>
+/// </summary>
+public class Solution
+{
+    public int RemoveDuplicates(int[] nums)
+    {
+        if (nums.Length < 2)
+        {
+            return nums.Length;
+        }
+
+        int idx = 0;
+        int vIdx = 0;
+        int n = nums.Length;
+        while (vIdx < n)
+        {
+            vIdx++;
+            if (vIdx >= n) break;
+            if (nums[vIdx] == nums[idx]) continue;
+            nums[idx + 1] = nums[vIdx];
+            ++idx;
+        }
+
+        return idx + 1;
+    }
+}
