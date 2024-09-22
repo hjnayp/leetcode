89fa62c94860b11f5a0a92e3e5a99491bc47e38a
Nayun <Han.Nayun@icloud.com>
2024-09-22 19:54:58 +0800

    + 997

---
+++ source/0900/997.cs
@@ -0,0 +1,41 @@
+﻿namespace source._0900._997;
+
+/// <summary>
+///     <a href="https://leetcode.cn/problems/find-the-town-judge">
+///         Find the Town Judge
+///     </a>
+/// </summary>
+public class Solution
+{
+    public int FindJudge(int n, int[][] trust)
+    {
+        IDictionary<int, int[]> idToTrustInfo = new Dictionary<int, int[]>();
+        foreach (int[] info in trust)
+        {
+            int a = info[0];
+            int b = info[1];
+            idToTrustInfo.TryAdd(a, [0, 0]);
+            idToTrustInfo.TryAdd(b, [0, 0]);
+            idToTrustInfo[a][0]++;
+            idToTrustInfo[b][1]++;
+        }
+
+        for (int i = 1; i <= n; ++i)
+        {
+            if (idToTrustInfo.TryGetValue(i, out int[]? info))
+            {
+                if (info[0] == 0 &&
+                    info[1] == n - 1)
+                {
+                    return i;
+                }
+            }
+            else if (n == 1)
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
---
+++ test/0900/Test997.cs
@@ -0,0 +1,29 @@
+﻿using JetBrains.Annotations;
+using source._0900._997;
+
+namespace test._0900;
+
+[TestClass, TestSubject(typeof(Solution))]
+public class Test997
+{
+    [TestMethod]
+    public void TestSolution_WhenNoJudge_ShouldReturnMinusOne()
+    {
+        RunTestCase(-1, 3, [[1, 3], [2, 3], [3, 1]]);
+        RunTestCase(-1, 3, [[1, 2]]);
+    }
+
+    [TestMethod]
+    public void TestSolution_WhenCommonCase()
+    {
+        RunTestCase(2, 2, [[1, 2]]);
+        RunTestCase(3, 3, [[1, 3], [2, 3]]);
+        RunTestCase(1, 1, []);
+    }
+
+    private static void RunTestCase(int expected, int n, int[][] trust)
+    {
+        Solution solution = new();
+        Assert.AreEqual(expected, solution.FindJudge(n, trust));
+    }
+}
