2a7ff4afb8e87a729790a619cf903d119eb234df
Nayun <Han.Nayun@icloud.com>
2024-10-08 21:31:39 +0800

    + 1227

---
+++ source/1200/1227.cs
@@ -0,0 +1,16 @@
+﻿namespace source._1200._1227;
+
+/// <summary>
+///     <a
+///         href="https://leetcode.cn/problems/airplane-seat-assignment-probability/description/?envType=daily-question&envId=2024-10-04">
+///         Airplane Seat Assignment Probability
+///     </a>
+/// </summary>
+public class Solution
+{
+    public double NthPersonGetsNthSeat(int n)
+    {
+        if (n == 1) return 1.0;
+        return 1.0 / n + (n - 2.0) / n * NthPersonGetsNthSeat(n - 1);
+    }
+}
---
+++ test/1200/Test1227.cs
@@ -0,0 +1,17 @@
+﻿using JetBrains.Annotations;
+using source._1200._1227;
+
+namespace test._1200;
+
+[TestClass, TestSubject(typeof(Solution))]
+public class Test1227
+{
+    [TestMethod]
+    [Timeout(1000)]
+    public void TestSolution()
+    {
+        Solution solution = new();
+        Assert.AreEqual(1.0, solution.NthPersonGetsNthSeat(1));
+        Assert.AreEqual(0.5, solution.NthPersonGetsNthSeat(2));
+    }
+}
