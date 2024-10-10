acf973dacc8bf24c47de434571dc2bf2238df8f6
nayun <HanNayun_china@outlook.com>
2024-10-10 10:53:00 +0800

    * Refactor with LINQ

--- source/1400/1436.cs
+++ source/1400/1436.cs
@@ -9,17 +9,7 @@ public class Solution
 {
     public string DestCity(IList<IList<string>> paths)
     {
-        HashSet<string> cities = new();
-        HashSet<string> visitedCities = new();
-        foreach (IList<string> path in paths)
-        {
-            string from = path[0];
-            string to = path[1];
-            cities.Add(from);
-            cities.Add(to);
-            visitedCities.Add(from);
-        }
-
-        return cities.First(city => !visitedCities.Contains(city));
+        HashSet<string> cities = paths.Select(p => p[0]).ToHashSet();
+        return paths.FirstOrDefault(p => !cities.Contains(p[1]), ["", ""])[1];
     }
 }
