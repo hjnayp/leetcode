049d1d820365ce04cb031fc0ecb318afef8b7703
nayun <HanNayun_china@outlook.com>
2024-10-30 18:25:00 +0800

    + 3216

---
+++ source/3200/3216.cs
@@ -0,0 +1,25 @@
+namespace source._3200._3216;
+
+/// <summary>
+///     <a href="https://leetcode.cn/problems/lexicographically-smallest-string-after-a-swap">
+///     </a>
+/// </summary>
+public class Solution
+{
+    public string GetSmallestString(string s)
+    {
+        for (var i = 0; i < s.Length - 1; i++)
+        {
+            var prevChar = s[i];
+            var nextChar = s[i + 1];
+
+            if (prevChar % 2 != nextChar % 2) continue;
+            if (prevChar > nextChar)
+            {
+                return s[..i] + nextChar + prevChar + s[(i + 2)..];
+            }
+        }
+
+        return s;
+    }
+}
