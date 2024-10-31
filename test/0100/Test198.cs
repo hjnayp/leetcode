36e5eb2859ac5d1729e0c19b96d9ee1501aef222
nayun <HanNayun_china@outlook.com>
2024-10-31 14:45:40 +0800

    + 198

---
+++ test/0100/Test198.cs
@@ -0,0 +1,24 @@
+using JetBrains.Annotations;
+using source._0100._198;
+
+namespace test._0100;
+
+[TestClass]
+[TestSubject(typeof(Solution))]
+public class Test198
+{
+    [TestMethod]
+    public void TestSolution()
+    {
+        var solution = new Solution();
+
+        int[] nums = [2, 1, 1, 2];
+        Assert.AreEqual(4, solution.Rob(nums));
+
+        nums = [1, 2, 3, 1];
+        Assert.AreEqual(4, solution.Rob(nums));
+
+        nums = [2, 7, 9, 3, 1];
+        Assert.AreEqual(12, solution.Rob(nums));
+    }
+}
