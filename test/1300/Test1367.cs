68a948c1e9d633d221a9cc0a6b443e6d0489e721
lifenowloading <lifenowloading@qq.com>
2025-01-03 10:48:28 +0800

    use new way to create list to do null check

--- test/1300/Test1367.cs
+++ test/1300/Test1367.cs
@@ -16,19 +16,19 @@ public class Test1367
         var root = TreeNode.CreateTreeWithList([
             1, 4, 4, null, 2, 2, null, 1, null, 6, 8, null, null, null, null, 1, 3,
         ]);
-        if (ListNode.CreateList([4, 2, 8], out var head))
+        if (ListNode.TryCreateList([4, 2, 8], out var head))
         {
             Assert.IsTrue(solution.IsSubPath(head, root));
         }
 
         root = TreeNode.CreateTreeWithList([1, 4, 4, null, 2, 2, null, 1, null, 6, 8, null, null, null, null, 1, 3]);
-        if (ListNode.CreateList([1, 4, 2, 6], out head))
+        if (ListNode.TryCreateList([1, 4, 2, 6], out head))
         {
             Assert.IsTrue(solution.IsSubPath(head, root));
         }
 
         root = TreeNode.CreateTreeWithList([1, 4, 4, null, 2, 2, null, 1, null, 6, 8, null, null, null, null, 1, 3]);
-        if (ListNode.CreateList([1, 4, 2, 6, 8], out head))
+        if (ListNode.TryCreateList([1, 4, 2, 6, 8], out head))
         {
             Assert.IsFalse(solution.IsSubPath(head, root));
         }
