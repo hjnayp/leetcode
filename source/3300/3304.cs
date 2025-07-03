4aa358ca2f447fc4c88f80ece4ef82cf160ac8d1
lifenowloading <lifenowloading@qq.com>
2025-07-03 11:02:47 +0800

    + use better way solve 3304

--- source/3300/3304.cs
+++ source/3300/3304.cs
@@ -1,39 +1,17 @@
-using System.Text;
+using System.Numerics;
 
 namespace source._3300._3304;
 
+/// <summary>
+///     <a href="https://leetcode.cn/problems/find-the-k-th-character-in-string-game-i">
+///         3304. Find the K-th Character in String Game I
+///     </a>
+/// </summary>
 public class Solution
 {
-    private static string s_words = "a";
-
-    private static void InitWord()
-    {
-        StringBuilder sb = new(s_words);
-        while (sb.Length < 500)
-        {
-            int len = sb.Length;
-            for (int i = 0; i < len; ++i)
-            {
-                sb.Append(GetNextChar(sb[i]));
-            }
-        }
-
-        s_words = sb.ToString();
-    }
-
     public char KthCharacter(int k)
     {
-        if (s_words.Length < 500) InitWord();
-        return s_words[k - 1];
-    }
-
-    private static char GetNextChar(char ch)
-    {
-        if (ch is 'z')
-        {
-            return 'a';
-        }
-
-        return (char)(ch + 1);
+        int count = BitOperations.PopCount((uint)(k - 1)) % 26;
+        return (char)('a' + count);
     }
 }
